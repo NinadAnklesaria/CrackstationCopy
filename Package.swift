640104acac9e36e05d949d9280a15d87a855edb7
Ninad1011 <54536456+Ninad1011@users.noreply.github.com>
2022-11-08 19:23:37 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CrackStation",
-            targets: ["CrackStation"]),
+            targets: ["CrackstationCopy"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
